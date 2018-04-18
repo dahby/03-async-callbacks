ffeaab5e91075f6d339dcd4d2ba0c1f1e8a348bc
dahby <dstoll243@gmail.com>
2018-04-18 15:57:38 -0700

    arrays are correctly mapping strings

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const logger = require('./lib/logger');
+const fileReader = require('./lib/reader');
+
+const onePath = `${__dirname}/data/one.txt`;
+const twoPath = `${__dirname}/data/two.txt`;
+const threePath = `${__dirname}/data/three.txt`;
+
+const files = [onePath, twoPath, threePath];
+
+const printCharacters = (characters) => {
+  console.log(characters);
+};
+
+const readFileArray = (fileArray, currentIndex, callback) => {
+  if (currentIndex >= fileArray.length) {
+    return callback();
+  }
+  const currentFilePath = fileArray[currentIndex];
+  try {
+    return fileReader.readFirst(currentFilePath, (file) => {
+      printCharacters(file);
+      readFileArray(fileArray, currentIndex + 1, callback);
+    });
+  } catch (error) {
+    logger.log(logger.ERROR, error);
+  }
+  return undefined;
+};
+
+readFileArray(files, 0, () => logger.log(logger.INFO, 'All files have been read'));
